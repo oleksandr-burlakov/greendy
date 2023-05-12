160989de4e7d2c9b63d9a4adf888cd76f1993225
oleksandr_burlakov <oleksandr.burlakov@intelini.com>
2023-05-12 12:38:58 +0300

    feat (rebuild db): remove items from preious project idea

--- backend/Greendy.DAL/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Greendy.DAL;
-public class Class1
-{
-
-}
--- db/create-db.sql
+++ db/create-db.sql
@@ -12,22 +12,6 @@ CREATE DATABASE greendy
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
--- Table: public.Tag
-
--- DROP TABLE IF EXISTS public."Tag";
-
-CREATE TABLE IF NOT EXISTS public."Tag"
-(
-    "TagId" uuid NOT NULL,
-    "Name" character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    "LastModifiedDate" timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "PK_Tag" PRIMARY KEY ("TagId")
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."Tag"
-    OWNER to postgres;
 
 -- Table: public.Role
 
@@ -44,23 +28,6 @@ CREATE TABLE IF NOT EXISTS public."Role"
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public."Role"
-    OWNER to postgres;
-
--- Table: public.Category
-
--- DROP TABLE IF EXISTS public."Category";
-
-CREATE TABLE IF NOT EXISTS public."Category"
-(
-    "CategoryId" uuid NOT NULL,
-    "Name" character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    "LastModifiedDate" timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "PK_Category" PRIMARY KEY ("CategoryId")
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."Category"
     OWNER to postgres;
 
 -- Table: public.User
@@ -86,28 +53,6 @@ TABLESPACE pg_default;
 ALTER TABLE IF EXISTS public."User"
     OWNER to postgres;
 
--- Table: public.Workspace
-
--- DROP TABLE IF EXISTS public."Workspace";
-
-CREATE TABLE IF NOT EXISTS public."Workspace"
-(
-    "WorkspaceId" uuid NOT NULL,
-    "OwnerId" uuid NOT NULL,
-    "Name" character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    "LastModifiedDate" timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "PK_Workspace" PRIMARY KEY ("WorkspaceId"),
-    CONSTRAINT "FK_Workspace_User" FOREIGN KEY ("OwnerId")
-        REFERENCES public."User" ("UserId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."Workspace"
-    OWNER to postgres;
-
 -- Table: public.UserRole
 
 -- DROP TABLE IF EXISTS public."UserRole";
@@ -131,66 +76,4 @@ CREATE TABLE IF NOT EXISTS public."UserRole"
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public."UserRole"
-    OWNER to postgres;
-
--- Table: public.Spending
-
--- DROP TABLE IF EXISTS public."Spending";
-
-CREATE TABLE IF NOT EXISTS public."Spending"
-(
-    "SpendingId" uuid NOT NULL,
-    "UserId" uuid NOT NULL,
-    "WorkspaceId" uuid NOT NULL,
-    "Amount" real NOT NULL,
-    "SpendingDate" timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
-    "Reason" text COLLATE pg_catalog."default",
-    "CategoryId" uuid,
-    "LastModifiedDate" timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "PK_Spending" PRIMARY KEY ("SpendingId"),
-    CONSTRAINT "FK_Spending_Category" FOREIGN KEY ("CategoryId")
-        REFERENCES public."Category" ("CategoryId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT "FK_Spending_User" FOREIGN KEY ("UserId")
-        REFERENCES public."User" ("UserId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT "FK_Spending_Workspace" FOREIGN KEY ("WorkspaceId")
-        REFERENCES public."Workspace" ("WorkspaceId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."Spending"
-    OWNER to postgres;
-
--- Table: public.SpendingTag
-
--- DROP TABLE IF EXISTS public."SpendingTag";
-
-CREATE TABLE IF NOT EXISTS public."SpendingTag"
-(
-    "SpendingTagId" uuid NOT NULL,
-    "SpendingId" uuid NOT NULL,
-    "TagId" uuid NOT NULL,
-    CONSTRAINT "PK_SpendingTag" PRIMARY KEY ("SpendingTagId"),
-    CONSTRAINT "FK_SpendingTag_Spending" FOREIGN KEY ("SpendingId")
-        REFERENCES public."Spending" ("SpendingId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT "FK_SpendingTag_Tag" FOREIGN KEY ("TagId")
-        REFERENCES public."Tag" ("TagId") MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."SpendingTag"
     OWNER to postgres;
